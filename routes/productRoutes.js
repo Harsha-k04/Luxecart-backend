5ae104ef446c1ad8488bfd92b0f74c8bf934526d
harshak04 <harsha332004@gmail.com>
2026-06-02 22:11:07 +0530

    Added admin role

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const Product = require("../models/Product");
 const protect = require("../middleware/authMiddleware");
+const admin = require("../middleware/adminMiddleware");
 
 
 // 🔹 GET all products (SEARCH + FILTER + SORT)
@@ -81,7 +82,7 @@ router.get("/", async (req, res) => {
 
 
 // 🔹 ADD product
-router.post("/", protect, async (req, res) => {
+router.post("/", protect, admin, async (req, res) => {
     try {
         const product = new Product(req.body);
 
@@ -196,5 +197,53 @@ router.get("/recommend/:id", async (req, res) => {
     }
 });
 
+// UPDATE PRODUCT
+router.put("/:id", protect, admin, async (req, res) => {
+    try {
+        const updatedProduct = await Product.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            {
+                new: true,
+            }
+        );
+
+        if (!updatedProduct) {
+            return res.status(404).json({
+                message: "Product not found",
+            });
+        }
+
+        res.json(updatedProduct);
+
+    } catch (error) {
+        res.status(500).json({
+            message: error.message,
+        });
+    }
+});
+// DELETE PRODUCT
+router.delete("/:id", protect, admin, async (req, res) => {
+    try {
+        const product = await Product.findById(req.params.id);
+
+        if (!product) {
+            return res.status(404).json({
+                message: "Product not found",
+            });
+        }
+
+        await product.deleteOne();
+
+        res.json({
+            message: "Product deleted",
+        });
+
+    } catch (error) {
+        res.status(500).json({
+            message: error.message,
+        });
+    }
+});
 
 module.exports = router;
